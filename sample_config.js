2aab6b7b3ce7fac385136dab3ed8b710e144fc8d
hussain <hussain.shaik2004@gmail.com>
2024-11-25 19:32:43 +0530

    added sample_config file

---
+++ sample_config.js
@@ -0,0 +1,9 @@
+const config = {
+    FIREBASE_AUTH_KEY : YOUR_API_KEY,
+    mSId: YOUR_messagingSenderId,
+    aId: YOUR_appId,
+    backendServer : `YOUR BACKEND SERVER URL`
+};
+export default config;
+
+// Replace your api keys and rename this file as config.js
